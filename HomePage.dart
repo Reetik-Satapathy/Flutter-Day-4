c1b57baa79bd19cae2cbc5f222fe5199c3b27615
Reetik Satapathy <124286397+Reetik-Satapathy@users.noreply.github.com>
2024-10-30 19:39:40 +0530

    Create HomePage.dart

---
+++ HomePage.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController username = TextEditingController();
+  final TextEditingController password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Login app"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 350,
+              child: TextFormField(
+                controller: username,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text("Username"),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              width: 350,
+              child: TextFormField(
+                controller: password,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text("Password"),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (username.text == "kjc" && password.text == "pass123") {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text("Login Successful!"),
+                      content: Text("You can go ahead"),
+                      actions: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text("Ok"),
+                        ),
+                      ],
+                    ),
+                  );
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text("Login Failed!"),
+                      content: Text("Check username or password"),
+                      actions: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("Ok"),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              },
+              child: Text("Login"),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
